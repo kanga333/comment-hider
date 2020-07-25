be6b35bc02a2078b44f87afcf0b03e7b69ef9e68
kanga333 <e411z7t40w@gmail.com>
2020-07-25 23:58:26 +0900

    Add tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,3 +1,22 @@
-test('noting', async () => {
-  //TODO
+import {Client} from '../src/client'
+import listComment from './list_comment.json'
+import * as path from 'path'
+import nock from 'nock'
+
+describe('Hide Comments', () => {
+  beforeEach(() => {
+    nock.cleanAll()
+    process.env.GITHUB_EVENT_PATH = path.join(__dirname, 'payload.json')
+  })
+
+  it('should only select the bot comment id', async () => {
+    const client = new Client('secrets', 'owner', 'repo', 1)
+    const github = nock('https://api.github.com')
+      .get(`/repos/owner/repo/issues/1/comments`)
+      .reply(200, listComment)
+
+    const response = await client.SelectComments(`bot`)
+
+    expect(response).toStrictEqual(['hide me'])
+  })
 })
